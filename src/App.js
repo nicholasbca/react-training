3e8df5b8fc3ffc3781bbe9dd832a5d36c7de9f3a
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-11 03:47:00 +0700

    update styling v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './css/style.css'
 import StatefullExample from './day1-practice/components/beforeHooks/statefull';
 import FetchDataHooks from './day1-practice/FetchHooks';
 import CounterApp from './day1-practice/counterApp';
@@ -8,8 +9,10 @@ import TransferApp from './day2-practice/components';
 function App() {
   return (
     <div className="App">
+      <div className="bg-gray-100 rounded-xl p-8 mt-10 mb-10 mx-80">
       <h2>Hello Everyone</h2>
-      <TransferApp />
+      <TransferApp />  
+      </div>
     </div>
   );
 }
