65f948875b7bb235062514ad05f96e8ea8529e2a
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-12 05:27:00 +0700

    update styling v3

--- src/day2-practice/inline/TitleComponent.jsx
+++ src/day2-practice/inline/TitleComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 //you can add export
 const divStyle = {
-  border:"5px solid blue", 
+  border:"5px solid gold", 
   width:"30%", 
   margin:"0 35% 0 35%", //top right bottom left
   borderRadius:'15px'
@@ -10,12 +10,8 @@ const divStyle = {
 
 export default function TitleComponent({text}) {
   return (
-    <div style={{
-      border:"5px solid blue", 
-      width:"30%", 
-      margin:"0 35% 0 35%", //top right bottom left
-      borderRadius:'15px'
-      }}>
+    // <div style={{border: "5px solid aqua", width:"20%", margin: "0 40%", borderRadius:"10px"}}>
+    <div style={divStyle}>
      {text}
     </div>
   )
