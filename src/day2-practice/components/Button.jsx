0d2092a4f01cfb5b347ee7e609a1c7b12e7edec2
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-12 04:21:00 +0700

    update styling v3

--- src/day2-practice/components/Button.jsx
+++ src/day2-practice/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ButtonKlik({text, color, klik}) {
   return (
     <div>
-      <button style={{background:color}} onClick={klik}>{text}</button>
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={klik}>{text}</button>
     </div>
   )
 }
