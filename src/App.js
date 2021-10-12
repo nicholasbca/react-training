1f53ebce351b8f66c9115bc4860bfec54d9cdd7b
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-13 04:33:00 +0700

    add practice day 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ReactRedux from './day4-practice/react-redux';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello Everyone</h2>
+      <ReactRedux />
     </div>
   );
 }
