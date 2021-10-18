7457fc2618c4b250270e11302d495f816e871e38
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-18 12:19:00 +0700

    updated restructure v1

--- src/Button.jsx
+++
@@ -1,8 +0,0 @@
-//es5 -> stateless
-export default function Button({color, func, sym}){ //destructuring
-  // const {color, func, sym} = props
-
-  return(
-    <button style={{background:color}} onClick={func}>{sym}</button>
-  )
-}
