d085d496c80408a753e1fc1ff3970a12ea51d434
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-20 07:43:00 +0700

    updated restructure v3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import StatefullExample from './components/beforeHooks/statefull';
-import FetchData from './components/fetch';
-import CounterApp from './counterApp'
-import FetchDataHooks from './FetchHooks';
-import CounterHooks from './Hooks/CounterHooks';
+import StatefullExample from './day1-practice/components/beforeHooks/statefull';
+import FetchDataHooks from './day1-practice/FetchHooks';
+import CounterApp from './day1-practice/counterApp';
+import CounterHooks from './day1-practice/Hooks/CounterHooks';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello Everyone</h2>
-      {/* <FetchDataHooks /> */}
-      <CounterHooks />
+      
+      
     </div>
   );
 }
