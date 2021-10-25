eef378be481946e9ecddae333672463c44c2d447
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-25 11:32:00 +0700

    integrate the fetch to the app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import StatefullExample from './components/beforeHooks/statefull';
+import FetchData from './components/fetch';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello Everyone</h2>
-      <StatefullExample />
+      {/* <StatefullExample /> */}
+      <FetchData />
     </div>
   );
 }
