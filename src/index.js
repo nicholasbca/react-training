4b512a1c6cc6ea83f4404029c6ca9bb46f6c2427
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-27 09:44:00 +0700

    update has roouter

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
