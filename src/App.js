8e78585540a385b7cf9ffecb9c62674369589761
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-10-28 17:09:00 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
+import React from 'react';
 import './css/style.css'
 import StatefullExample from './day1-practice/components/beforeHooks/statefull';
 import FetchDataHooks from './day1-practice/FetchHooks';
 import CounterApp from './day1-practice/counterApp';
 import CounterHooks from './day1-practice/Hooks/CounterHooks';
 import TransferApp from './day2-practice/components';
+import RouterApp from './day3-practice/Routeer';
 
 function App() {
   return (
     <div className="App">
-      <div className="bg-gray-100 rounded-xl p-8 mt-10 mb-10 mx-80">
-      <h2>Hello Everyone</h2>
-      <TransferApp />  
-      </div>
+      {/* Hello Everyone */}
+      {/* <TransferApp /> */}
+      <RouterApp />
     </div>
   );
 }
--- src/day2-practice/components/index.js
+++ src/day2-practice/components/index.js
@@ -1,9 +1,42 @@
-import React, {Fragment, useState} from 'react'
+import React, {Fragment, useEffect, useState} from 'react'
 import TitleComponent from '../inline/TitleComponent'
 import ButtonKlik from './Button'
 
 export default function TransferApp() {
   const [balance, setBalance] = useState(0)
+  const [members, setMembers] = useState([])
+  const [nohp, setNohp] = useState([])
+  const [users, setUsers] = useState([])
+
+  const getUser = async () =>{
+    try {
+      const raw = await fetch('http://jsonplaceholder.typicode.com/users')
+      const data = await raw.json()
+      // console.log(data)
+      const names = data.map(a => a.name) //ini manggil property name
+      const phones = data.map(a => a.phone)
+      setMembers(names)
+      setNohp(phones)
+      setUsers(data)
+      // const names = 
+    } catch (error) {
+      console.log('Data Error')
+    }
+  }
+
+  useEffect(() => {
+    // loadUser()
+  }, [])
+
+  const loadUser = () => {
+    setTimeout(() => {
+      alert('load data')
+      getUser()
+    }, 5000)
+  }
+
+  console.log('nama kontak', members)
+  console.log('no hp kontak', nohp)
 
   const save1000 = (value) => {
     setBalance(balance + value)
@@ -35,6 +68,7 @@ export default function TransferApp() {
 
   return (
     <Fragment>
+      <div className="bg-gray-100 rounded-xl p-8 mt-10 mb-10 mx-80">
       <TitleComponent text={`Saldo anda adalah : Rp. ${balance}`} /> 
       {/* Saldo anda adalah : Rp. {balance} */}
       <br />
@@ -48,6 +82,12 @@ export default function TransferApp() {
       <br/>
       {/* <button onClick={() => this.tarikSaldo1000()}>Tarik Rp.1000</button> */}
       <ButtonKlik klik={() => withdrw1000(1000)} text={"Tarik Rp. 1000"} color={"Red"}/>
+      <div className="space-y-4 mt-8">
+        {users.map((user, id) => (
+          <span className="bg-blue-200 block rounded-md" key={id}>{user.name} {user.phone}</span>
+        ))}
+      </div>
+      </div>
     </Fragment>
   )
 }
